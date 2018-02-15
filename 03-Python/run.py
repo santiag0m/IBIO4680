9eec979eebf96f5d2538618e5b3f95f089741380
santiag0m <32421689+santiag0m@users.noreply.github.com>
2018-02-15 00:47:44 -0500

    Updated run.py

--- 03-Python/run.py
+++ 03-Python/run.py
@@ -9,9 +9,13 @@ from subprocess import call # Run shell commands
 
 # Dataset download
 
-if ~os.direxists(os.path.join(os.getcwd(),'BSR')):
+if not os.path.isdir(os.path.join(os.getcwd(),'BSR')):
     call(['wget','http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz'])
     call(['tar','-xf','BSR_bsds500.tgz'])
+
+# Begin timer
+
+tini = time.time()
 
 # Dataset path
 
@@ -40,7 +44,18 @@ idx = np.random.randint(0,n-1,7) # 7 random images
 
 selected = []
 
-for file in images:
-    img = cv2.imread(os.path.join(imagepath,file))
+for iidx in idx:
+    img = cv2.imread(os.path.join(imagepath,images[iidx]))
     img = cv2.resize(img,(256,256))
-    #mdict = ['image':img,'truth':label]
+    matvar = scipy.io.loadmat(os.path.join(truthpath,truth[iidx]))
+    label = matvar['groundTruth']
+    mdict = ['images':img,'truth':label]
+    selected.append(mdict)
+
+f = open('myimages','wb')
+pickle.dump(selected,f)
+f.close()
+
+# End timer
+
+print(time.time()-tini)
