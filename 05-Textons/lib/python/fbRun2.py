c1dc78d5e300a9abab9af077138a78e0845a17fd
santiag0m <cristinaraul@ing-104.ad.uniandes.edu.co>
2018-02-26 16:50:29 -0500

    changes in python library

---
+++ 05-Textons/lib/python/fbRun2.py
@@ -0,0 +1,32 @@
+import math
+import imageio
+import numpy as np
+import scipy.signal
+from padReflect import padReflect
+
+def fbRun(fb,im):
+
+
+    #find the max filter size
+    maxsz = fb[0][0].shape
+    for filter in fb:
+        for scale in filter:
+            maxsz = max(maxsz, scale.shape)
+    maxsz = maxsz[0]
+
+	#pad the image 
+    r = int(math.floor(maxsz/2))
+    impad = padReflect(im[:64,:64],r)
+    #impad = impad[0:2*r,0:2*r] # Test
+
+	#run the filterbank on the padded image, and crop the result back
+	#to the original image size
+    fim = np.zeros(np.array(fb).shape).tolist()
+    for i in range(np.array(fb).shape[0]):
+        for j in range(np.array(fb).shape[1]):
+            if fb[i][j].shape[0]<50:
+                fim[i][j] = scipy.signal.convolve2d(impad, fb[i][j], 'same')
+            else:
+                fim[i][j] = fftconvolve(impad,fb[i][j])
+            fim[i][j] = fim[i][j][0:2*r,0:2*r]
+    return fim
