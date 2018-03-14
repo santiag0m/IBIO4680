4fcceb3ccc2fb686e9afd357540acac5d6b565c7
santiag0m <vision@ing-458.ad.uniandes.edu.co>
2018-03-14 17:54:44 -0500

    updated

--- 07-BSDS/runMethods.m
+++ 07-BSDS/runMethods.m
@@ -1,5 +1,7 @@
-imDir = fullfile(pwd,'images','test');
-outDir = fullfile(pwd,'segs');
+
+globDir = '/datos1/vision/GonzalezMolina/BSR/BSDS500/data'
+imDir = fullfile(globDir,'images','test');
+outDir = fullfile(globDir,'segs');
 mkdir(outDir)
 
 k = 10;
@@ -7,4 +9,11 @@ k = logspace(1,3,k);
 k = round(k);
 
 fl = ls(imDir);
-fl(1:2,:) = [];
+fl = strsplit(fl);
+fl(1:2) = [];
+
+for i=1:numel(fl)
+    % Segmentation matrix
+    segs = cell(10,1);
+
+
