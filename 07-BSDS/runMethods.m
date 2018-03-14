6fae7fb0d1cb48b970d9cdab55072f8af79f07fc
santiag0m <vision@ing-458.ad.uniandes.edu.co>
2018-03-14 18:20:42 -0500

    updated functin

--- 07-BSDS/runMethods.m
+++ 07-BSDS/runMethods.m
@@ -1,8 +1,10 @@
 
 globDir = '/datos1/vision/GonzalezMolina/BSR/BSDS500/data'
 imDir = fullfile(globDir,'images','test');
-outDir = fullfile(globDir,'segs');
-mkdir(outDir)
+outDir1 = fullfile(globDir,'segs','function1');
+mkdir(outDir1)
+outDir2 = fullfile(globDir,'segs','function2');
+mkdir(outDir2)
 
 k = 10;
 k = logspace(1,3,k);
@@ -14,6 +16,28 @@ fl(1:2) = [];
 
 for i=1:numel(fl)
     % Segmentation matrix
-    segs = cell(10,1);
+    segs = cell(numel(k),1);
+    imname = fl{i};
+    img = imread(fullfile(imDir,imname));
+    for w=1:2
+        switch w
+            case 1
+            matname = fullfile(outDir1,strcat(imname(1:end-4),'.mat'));
+            segfun = 'gmm';
+            case 2
+            matname = fullfile(outDir2,strcat(imname(1:end-4),'.mat'));
+            segfun = 'hierarchical';
+            img = imresize(img,0.5) % Run faster
+        end
+        matname = fullfile(outDir,strcat(imname(1:end-4),'.mat'));
+        for j=1:numel(k)
+            segs{j}=segmentByClustering(img,'hsv',segfun,k(j));
+            if w==2
+                segs{j}=imresize(segs{j},2,'nearest');
+            end
+        end
+        save(matname,'segs');
+    end
+end
 
 
